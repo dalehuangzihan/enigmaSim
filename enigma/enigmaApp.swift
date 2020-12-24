2409ee0869c5ab73407ac1c99abdaf2c3d9a5922
Dale Huang <huangzihan98@gmail.com>
2020-12-24 14:27:14 +0800

    initial commit

---
+++ enigma/enigmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  enigmaApp.swift
+//  enigma
+//
+//  Created by Dale Huang on 24/12/20.
+//
+
+import SwiftUI
+
+@main
+struct enigmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
